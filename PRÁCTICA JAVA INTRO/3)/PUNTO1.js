4ebd1b0147f2d1b1785e21877ee7cf6e3115ed99
Valentino Maria Laiño <v.laino57@gmail.com>
2026-08-16 20:16:28 -0300

    Punto 3 a medias

---
+++ PRÁCTICA JAVA INTRO/3)/PUNTO1.js
@@ -0,0 +1,5 @@
+// Consumo de Datos desde una API:
+//○ Crea una función llamada obtenerUsuarios que haga una petición
+//HTTP a la API https://jsonplaceholder.typicode.com/users usando
+//fetch. Luego, imprime en la consola la lista de usuarios obtenida.
+
